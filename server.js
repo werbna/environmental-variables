a58b572f40160577c3f96a22bb2ac06d74f13646
werbnah <andrewchau1993@gmail.com>
2024-07-24 11:31:22 -0700

    test

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+  res.send('The Server is Running');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+  console.log(`Your secret is ${process.env.SECRET_PASSWORD}`)
+});
